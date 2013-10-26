2f093716b48c1b06ff80f59f83b4c58cbd9c782d
obelaifa <oliver@belaifa.de>
2013-10-26 19:14:04 +0200

    IDs used to identify the skeleton joints

---
+++ tuiframework/cpp/tuilibs/tuitypes/common/SkeletonJointID.h
@@ -0,0 +1,58 @@
+/*
+    Copyright (C) 2010, 2011, 2012 The Fraunhofer Institute for Production Systems and
+    Design Technology IPK. All rights reserved.
+
+    This file is part of the TUIFramework library.
+    It includes a software framework which contains common code
+    providing generic functionality for developing applications
+    with a tangible user interface (TUI).
+    
+    The TUIFramework library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    The TUIFramework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the TUIFramework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#ifndef _SkeletonJointID_h_
+#define _SkeletonJointID_h_
+
+#define SKEL_HEAD               101
+#define SKEL_NECK               102
+#define SKEL_TORSO              103
+#define SKEL_WAIST              104
+
+#define SKEL_LEFT_COLLAR        105
+#define SKEL_LEFT_SHOULDER      106
+#define SKEL_LEFT_ELBOW         107
+#define SKEL_LEFT_WRIST         108
+#define SKEL_LEFT_HAND          109
+#define SKEL_LEFT_FINGERTIP     110
+
+#define SKEL_RIGHT_COLLAR       111
+#define SKEL_RIGHT_SHOULDER     112
+#define SKEL_RIGHT_ELBOW        113
+#define SKEL_RIGHT_WRIST        114
+#define SKEL_RIGHT_HAND         115
+#define SKEL_RIGHT_FINGERTIP    116
+
+#define SKEL_LEFT_HIP           117
+#define SKEL_LEFT_KNEE          118
+#define SKEL_LEFT_ANKLE         119
+#define SKEL_LEFT_FOOT          120
+
+#define SKEL_RIGHT_HIP          121
+#define SKEL_RIGHT_KNEE         122
+#define SKEL_RIGHT_ANKLE        123
+#define SKEL_RIGHT_FOOT         124        
+
+#endif
