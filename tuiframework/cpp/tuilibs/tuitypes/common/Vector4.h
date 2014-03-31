185e221d0b97bb1cc66429b94396eae5ac2d06fd
Oliver Belaifa <obelaifa@IPK470.(none)>
2014-03-31 14:06:28 +0200

    Vector3 -> Vector4, added implementation for homogenius parameter w

--- tuiframework/cpp/tuilibs/tuitypes/common/Vector4.h
+++ tuiframework/cpp/tuilibs/tuitypes/common/Vector4.h
@@ -21,138 +21,178 @@
     along with the TUIFramework.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _tuiframework_Vector3_h_
-#define _tuiframework_Vector3_h_
+#ifndef _tuiframework_Vector4_h_
+#define _tuiframework_Vector4_h_
 
 #include <tuiframework/core/ISerializable.h>
 #include <iostream>
 
 template <typename T>
-class Vector3 : public tuiframework::ISerializable {
+class Vector4 : public tuiframework::ISerializable {
 public:
-    Vector3() {
+    Vector4() {
+        this->data[0] = 0;
+        this->data[1] = 0;
+        this->data[2] = 0;
+        this->data[3] = 1.0;
     }
     
-    
-    Vector3(const Vector3<T> & v) {
+    Vector4(const Vector4<T> & v) {
         this->setData(v.getData());
     }
     
-    
-    inline Vector3(T x, T y, T z) {
+    inline Vector4(T x, T y, T z) {
         this->data[0] = x;
         this->data[1] = y;
         this->data[2] = z;
+        this->data[3] = 1.0;
     }
     
-    
-    ~Vector3() {
+    inline Vector4(T x, T y, T z, T w) {
+        this->data[0] = x;
+        this->data[1] = y;
+        this->data[2] = z;
+        this->data[3] = w;
     }
     
+    ~Vector4() {
+    }
     
     inline const T * getData() const {
         return this->data;
     }
     
-    
     inline void setData(const T * data) {
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < 4; ++i) {
             this->data[i] = data[i];
         }
     }
     
+    inline void set(T x, T y, T z, T w) {
+        this->data[0] = x;
+        this->data[1] = y;
+        this->data[2] = z;
+        this->data[3] = w;
+    }
     
     inline const T & operator [](int i) const {
         return this->data[i];
     }
     
-    
     inline T & operator [](int i) {
         return this->data[i];
     }
     
-    
-    inline static T dotProduct(const Vector3<T> & a, const Vector3<T> & b) {
-        return
-        a[0]*b[0] +
-        a[1]*b[1] +
-        a[2]*b[2];
+    inline static T dotProduct(const Vector4<T> & a, const Vector4<T> & b) {
+        return (a[0]*b[0] + a[1]*b[1] + a[2]*b[2])*a[3]*b[3];
     }
     
-    
-    inline static Vector3<T> scale(const T & a, const Vector3<T> & b) {
-        return Vector3<T>(a*b[0], a*b[1], a*b[2]);
+    inline static Vector4<T> scale(const T & a, const Vector4<T> & b) {
+        return Vector4<T>(a*b[0], a*b[1], a*b[2]);
     }
     
+    inline void scale(T factor) {
+        for (int i = 0; i < 3; ++i) {
+            this->data[i] *= factor;
+        }
+    }
     
-    inline static Vector3<T> crossProduct(const Vector3<T> & a, const Vector3<T> & b) {
-        Vector3<T> c;
+    inline static Vector4<T> crossProduct(const Vector4<T> & a, const Vector4<T> & b) {
+        Vector4<T> c;
         c[0] = a[1]*b[2] - a[2]*b[1];
         c[1] = a[2]*b[0] - a[0]*b[2];
         c[2] = a[0]*b[1] - a[1]*b[0];
+        c[3] = a[3]*b[3];
         return c;
     }
     
-    
-    inline Vector3<T> & operator +=(const Vector3<T> & a) {
+    inline static Vector4<T> times(const Vector4<T> & a, const Vector4<T> & b) {
+        Vector4<T> c;
         for (int i = 0; i < 4; ++i) {
-            this->data[i] += a.data[i];
+            c.data[i] = a.data[i]*b.data[i];
+        }
+        return c;
+    }
+
+    inline Vector4<T> & operator +=(const Vector4<T> & a) {
+        for (int i = 0; i < 3; ++i) {
+            this->data[i] += a.data[i]*a[3]/this->data[3];
         }
         return *this;
     }
     
-    
-    inline Vector3<T> & operator -=(const Vector3<T> & a) {
-        for (int i = 0; i < 4; ++i) {
-            this->data[i] -= a.data[i];
+    inline Vector4<T> & operator -=(const Vector4<T> & a) {
+        for (int i = 0; i < 3; ++i) {
+            this->data[i] -= a.data[i]*a[3]/this->data[3];
         }
         return *this;
     }
     
+    inline T getAbs() const {
+        return static_cast<T>(sqrt(this->dotProduct(*this, *this)));
+    }
+    
+    inline void scaleToAbs(T factor) {
+        T a = getAbs();
+        if (a < 0.000000001) {
+            return;
+        }
+        
+        T f = factor/a;
+        for (int i = 0; i < 3; ++i) {
+            this->data[i] *= f;
+        }
+    }
+    
+    inline void normalize() {
+        for (int i = 0; i < 3; ++i) {
+            this->data[i] *= this->data[3];
+        }
+        this->data[3] = 1;
+    }
     
     virtual std::ostream & serialize(std::ostream & os) const {
         os << this->data[0];
-        for (int i = 1; i < 3; ++i) {
+        for (int i = 1; i < 4; ++i) {
             os << " " << this->data[i];
         }
         return os;
     }
     
-    
     virtual std::istream & deSerialize(std::istream & is) {
         is >> this->data[0];
-        for (int i = 1; i < 3; ++i) {
+        for (int i = 1; i < 4; ++i) {
             is >> this->data[i];
         }
         return is;
     }
     
-private:
-    T data[3];
+protected:
+    T data[4];
 };
 
 
 template <typename T>
-inline T operator *(const Vector3<T> & a, const Vector3<T> & b) {
-    return Vector3<T>::dotProduct(a, b);
+inline T operator *(const Vector4<T> & a, const Vector4<T> & b) {
+    return Vector4<T>::dotProduct(a, b);
 }
 
 
 template <typename T>
-inline Vector3<T> operator *(const T & a, const Vector3<T> & b) {
-    return Vector3<T>::scale(a, b);
+inline Vector4<T> operator *(const T & a, const Vector4<T> & b) {
+    return Vector4<T>::scale(a, b);
 }
 
 
 template <typename T>
-inline Vector3<T> operator *(const Vector3<T> & b, const T & a) {
-    return Vector3<T>::scale(a, b);
+inline Vector4<T> operator *(const Vector4<T> & b, const T & a) {
+    return Vector4<T>::scale(a, b);
 }
 
 
 template <typename T>
-inline Vector3<T> operator +(const Vector3<T> & a, const Vector3<T> & b) {
-    Vector3<T> c;
+inline Vector4<T> operator +(const Vector4<T> & a, const Vector4<T> & b) {
+    Vector4<T> c;
     c[0] = a[0] + b[0];
     c[1] = a[1] + b[1];
     c[2] = a[2] + b[2];
@@ -161,8 +201,8 @@ inline Vector3<T> operator +(const Vector3<T> & a, const Vector3<T> & b) {
 
 
 template <typename T>
-inline Vector3<T> operator -(const Vector3<T> & a, const Vector3<T> & b) {
-    Vector3<T> c;
+inline Vector4<T> operator -(const Vector4<T> & a, const Vector4<T> & b) {
+    Vector4<T> c;
     c[0] = a[0] - b[0];
     c[1] = a[1] - b[1];
     c[2] = a[2] - b[2];
