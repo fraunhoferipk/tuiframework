6db1086424b1d59b62f9515de7df36070f41af29
obelaifa <oliver@belaifa.de>
2014-11-11 00:42:41 +0100

    updated Matrix4OpPlugin

--- tuiframework/cpp/Windows/TUILibMatrix4OpDLL/Matrix4OpPluginWin32.cpp
+++ tuiframework/cpp/Windows/TUILibMatrix4OpDLL/Matrix4OpPluginWin32.cpp
@@ -5,9 +5,17 @@
 #include <tuiframework/server/mspConfig.h>
 #include <tuiframework/server/IMSPFactory.h>
 
-#include <matrix4op/Matrix4MulMSP.h>
-#include <matrix4op/Matrix4TranslMSP.h>
-
+#include <matrix4op/Matrix4DuplicateMSP.h>
+#include <matrix4op/Matrix4GetTranslationMSP.h>
+#include <matrix4op/Matrix4InverseMSP.h>
+#include <matrix4op/Matrix4MultiplicationMSP.h>
+#include <matrix4op/Matrix4PackedDuplicateMSP.h>
+#include <matrix4op/Matrix4SetTranslationMSP.h>
+#include <matrix4op/Matrix4TimesMSP.h>
+#include <matrix4op/Vector4DuplicateMSP.h>
+#include <matrix4op/Vector4Matrix4MultiplicationMSP.h>
+#include <matrix4op/Vector4PackedDuplicateMSP.h>
+#include <matrix4op/Vector4TimesMSP.h>
 
 #include <vector>
 
@@ -18,10 +26,30 @@ namespace tuiframework {
 static std::vector<std::string> mspTypeNameVector;
 
 void dllInitPlugin() {
-	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4MulMSP::getMSPTypeName(), Matrix4MulMSP::createFunction);
-	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4TranslMSP::getMSPTypeName(), Matrix4TranslMSP::createFunction);
-    mspTypeNameVector.push_back(Matrix4MulMSP::getMSPTypeName());
-	mspTypeNameVector.push_back(Matrix4TranslMSP::getMSPTypeName());
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4DuplicateMSP::getMSPTypeName(), Matrix4DuplicateMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4GetTranslationMSP::getMSPTypeName(), Matrix4GetTranslationMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4InverseMSP::getMSPTypeName(), Matrix4InverseMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4MultiplicationMSP::getMSPTypeName(), Matrix4MultiplicationMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4PackedDuplicateMSP::getMSPTypeName(), Matrix4PackedDuplicateMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4SetTranslationMSP::getMSPTypeName(), Matrix4SetTranslationMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Matrix4TimesMSP::getMSPTypeName(), Matrix4TimesMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Vector4DuplicateMSP::getMSPTypeName(), Vector4DuplicateMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Vector4Matrix4MultiplicationMSP::getMSPTypeName(), Vector4Matrix4MultiplicationMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Vector4PackedDuplicateMSP::getMSPTypeName(), Vector4PackedDuplicateMSP::createFunction);
+	MSPFactorySingleton::getInstance()->registerCreateFunction(Vector4TimesMSP::getMSPTypeName(), Vector4TimesMSP::createFunction);
+
+
+    mspTypeNameVector.push_back(Matrix4DuplicateMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Matrix4GetTranslationMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Matrix4InverseMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Matrix4MultiplicationMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Matrix4PackedDuplicateMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Matrix4SetTranslationMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Matrix4TimesMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Vector4DuplicateMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Vector4Matrix4MultiplicationMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Vector4PackedDuplicateMSP::getMSPTypeName());
+	mspTypeNameVector.push_back(Vector4TimesMSP::getMSPTypeName());
 }
 
 
@@ -36,8 +64,17 @@ std::string dllGetDLLVersion() {
 
 
 void dllMSPRegistration(IMSPFactory * MSPFactory) {
-    MSPFactory->registerCreateFunction(Matrix4MulMSP::getMSPTypeName(), Matrix4MulMSP::createFunction);
-	MSPFactory->registerCreateFunction(Matrix4TranslMSP::getMSPTypeName(), Matrix4TranslMSP::createFunction);
+    MSPFactory->registerCreateFunction(Matrix4DuplicateMSP::getMSPTypeName(), Matrix4DuplicateMSP::createFunction);
+	MSPFactory->registerCreateFunction(Matrix4GetTranslationMSP::getMSPTypeName(), Matrix4GetTranslationMSP::createFunction);
+	MSPFactory->registerCreateFunction(Matrix4InverseMSP::getMSPTypeName(), Matrix4InverseMSP::createFunction);
+	MSPFactory->registerCreateFunction(Matrix4MultiplicationMSP::getMSPTypeName(), Matrix4MultiplicationMSP::createFunction);
+	MSPFactory->registerCreateFunction(Matrix4PackedDuplicateMSP::getMSPTypeName(), Matrix4PackedDuplicateMSP::createFunction);
+	MSPFactory->registerCreateFunction(Matrix4SetTranslationMSP::getMSPTypeName(), Matrix4SetTranslationMSP::createFunction);
+	MSPFactory->registerCreateFunction(Matrix4TimesMSP::getMSPTypeName(), Matrix4TimesMSP::createFunction);
+	MSPFactory->registerCreateFunction(Vector4DuplicateMSP::getMSPTypeName(), Vector4DuplicateMSP::createFunction);
+	MSPFactory->registerCreateFunction(Vector4Matrix4MultiplicationMSP::getMSPTypeName(), Vector4Matrix4MultiplicationMSP::createFunction);
+	MSPFactory->registerCreateFunction(Vector4PackedDuplicateMSP::getMSPTypeName(), Vector4PackedDuplicateMSP::createFunction);
+	MSPFactory->registerCreateFunction(Vector4TimesMSP::getMSPTypeName(), Vector4TimesMSP::createFunction);
 }
 
 
